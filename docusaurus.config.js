f25f2eaf92ced5199b29df77d8128874fe8ce274
mffap <mpa@zitadel.com>
2025-03-22 18:10:22 +0200

    sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,6 +94,7 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      sidebarCollapsed: false,
       navbar: {
         title: 'AboutAuth',
         logo: {
