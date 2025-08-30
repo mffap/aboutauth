18371bb7f75f54c00a64eb85aac872750fc8af59
Maximilian Panne <mpa@zitadel.com>
2025-08-30 08:50:51 +0200

    as list

--- src/components/SoftwareTable/index.js
+++ src/components/SoftwareTable/index.js
@@ -1,40 +1,39 @@
 import Link from "@docusaurus/Link";
 import SoftwareProviders from "./software";
+import styles from './styles.module.css';
+
 import React from "react";
 
 export default function SoftwareTable() {
 
   const opt = { open_source_only: false };
-  const processors = SoftwareProviders(opt);
+  const providers = SoftwareProviders(opt);
 
   return (
-    <table className="text-xs">
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Cloud Service</th>
-        <th>Self Hosted</th>
-        <th>Open Source</th>
-      </tr>
+    < >
       {
-        processors
-          .sort((a, b) => {
+        providers
+          .sort((a,b) => {
             if (a.title < b.title) return -1
             if (a.title > b.title) return 1
             else return 0
           })
-          .map((processor, rowID) => {
-          return (
-            <tr>
-              <td key={rowID}><Link href={processor.link}>{processor.title}</Link></td>
-              <td>{processor.description}</td>
-              <td>{processor.cloud_service ? 'Yes'  : 'No'}</td>
-              <td>{processor.self_hosted ? 'Yes'  : 'No'}</td>
-              <td>{processor.open_source ? 'Yes'  : 'No'}</td>
-            </tr>
-          )
-        })
+          .map((provider, idx) => (
+            <SoftwareProvider key={idx} provider={provider} />
+          ))
       }
-    </table>
+    </ >
+  );
+}
+
+function SoftwareProvider({ provider }) {
+  return (
+    <section className={styles.providerSection}>
+      <h4>{provider.title}</h4>
+      <div>{provider.description}</div>
+      <div>
+        <Link href={provider.link}>{provider.title}</Link>
+      </div>
+    </section>
   );
 }
